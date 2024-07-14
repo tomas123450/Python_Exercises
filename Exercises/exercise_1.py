d49a09b06bc3ec9d51d42f8ffacf7c80620fcdc0
Tomas Gedminas <tomasgedminas05@gmail.com>
2024-07-14 11:51:02 +0300

    List comp and better practice

--- Exercises/exercise_1.py
+++ Exercises/exercise_1.py
@@ -1,32 +1,3 @@
-# #5.1. More on Lists
-# # 1_ex count,index,reverse,append,sort,pop
-def main():
- fruits = ["apple","banana","kiwi","orange","orange","apple","pear"]
- number_of_fruits = fruits.count("apple")
- print(fruits.index("banana"))
- print(number_of_fruits)
- fruits.insert(2,"insertinam")
- print(fruits)
- fruits.reverse()
- print(fruits)
- fruits.append("kiwi")
- print(fruits)
- fruits_copy = fruits.copy()
- print(fruits_copy)
- fruits.sort()
- print(fruits)
- fruits.pop()
- print(fruits)
-
-# 5.1.1. Using Lists as Stacks
-# 2_ex 
-# stack = [3, 4, 5]
-# stack.append("9")
-# stack.append("10")
-# stack.append("11")
-# stack.pop()
-# print(stack)
-
 
 #5.1.2. Using Lists as Queues
 # 3_ex
@@ -43,70 +14,37 @@ def main():
 
 # 5.1.3. List Comprehensions
 # squares = []
+# """Create a list of the first letter of each word in a given list: - """
+# words = ["apple", "banana", "cherry", "date"]
 
-# for i in range(10):
-#  squares.append[i**2]
+# """Create a list of the lengths of each word in a given sentence: NEZINAU split()"""
+# sentence = "The quick brown fox jumps over the lazy dog"
 
-# print(squares)
-
-#Lambda function def
-# numbers = [x+1 for x in range(10)]
-# print(numbers)
-
-# even_numbers = [num for num in range(10)]
-# print(even_numbers) 
-
-""" EXERCISE
-Based on a list of fruits, you want a new list, containing only the fruits with the letter "a" in the name.
-
-Without list comprehension you will have to write a for statement with a conditional test inside:
-Only accept items that are "apple":
-"""
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# #Lambda function def
+# squared = map(lambda x: x**2, [1,2,3])
+# print(squared)
 
 
-
-# get all even numbers from 1-50
-
-# numbers = [x for x in range(1,51) if x%2 == 0] 
-# print(numbers)
-
-###
-# options = ["any", "albany", "apple", "world", "hello", ""]
-
-# new_options = [word for word in options if word.startswith('a') and word.endswith('y')]
-# print(new_options)
-
-### # Flattening a matrix (list of lists)
-# matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-# flat_matrix = [num for row in matrix for num in row]
-
-#### Categorize numbers as even or odd IF ELSE IN A COMPREHENSION
+# #### Categorize numbers as even or odd IF ELSE IN A COMPREHENSION
 # numbers = [x for x in range(1,10)]
 
 # new_categorie = ["Even" if x%2 == 0 else "Odd" for x in numbers]
-# print(new_categorie)
+# divisible = [(x,"Divisible") if x%3 == 0 else (x,"Not divisible by 3")for x in numbers]
+# print(divisible)
 
-##### List comp with functions
-# def square(x):
-#  return x**2
 
-# squared_numbers = [square(x) for x in range(10)]
-# print(squared_numbers)
-
-### DICT COMPREHENSION
+# ## DICT COMPREHENSION
 # # Creating a dictionary
 # pairs = [("a", 1), ("b", 2), ("c", 3)]
-# my_dict = {k: v for k, v in pairs}
+# my_dict = {k:v for k,v in pairs}
 # print(my_dict)
 
 
-### SET COMPREHENSION
-# # Removing duplicates from a list while applying a function
-# nums = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
-# unique_squares = {x**2 for x in nums}
-# print(unique_squares)
+## SET COMPREHENSION
+# Removing duplicates from a list while applying a function
+nums = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+unique_squares = {x**2 for x in nums}
+print(unique_squares)
 
 
 
@@ -143,5 +81,5 @@ fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 
 
 ### NESTED LIST COMPREHENSION
-if __name__ == "__main__":
- main()
+# if __name__ == "__main__":
+#  main()
